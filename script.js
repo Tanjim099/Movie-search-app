2db0f56d29290a234a6b72df7018ab8de764164c
Tanjim <tanjimseo@gmail.com>
2023-08-19 15:54:06 +0530

    responsive done

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiKey = '5f0ff536';
 const searchButton = document.getElementById('searchButton');
 const searchInput = document.getElementById('searchInput');
 const movieCards = document.getElementById('movieCards');
+const mMovieCards = document.getElementById('m-movieCards');
 
 searchButton.addEventListener('click', searchMovie);
 console.log("Yes")
@@ -10,6 +11,8 @@ console.log("Yes")
 function searchMovie() {
     const searchTerm = searchInput.value.trim();
     if (searchTerm === '') return;
+    const movieBox = document.getElementById('movieBox');
+    movieBox.style.display = "block"
     const apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`;
 
     fetch(apiUrl)
@@ -18,21 +21,45 @@ function searchMovie() {
         .then(data => {
             console.log(data)
             if (data.Response === 'True') {
-                data.forEach(element => {
-                    // console.log(element)
-                });
+                // data.forEach(element => {
+                //     // console.log(element)
+                // });
                 console.log("Yes")
-                const { Title, Year, Plot, Poster } = data;
+                const { Title, Year, Plot, Poster, Genre, Language, Actors, Director, Writer, Released, imdbRating } = data;
                 console.log(Title)
                 movieCards.innerHTML = `
+                <div class="movieDetails">
+                <h2 class="title">${Title} (${Year})</h2>
+                <P><span>Actors: </span> ${Actors}</p>
+                <P><span>Director: </span> ${Director}</p>
+                <P><span>Writer: </span> ${Writer}</p>
+                <P><span>Released: </span> ${Released}</p>
+                <P><span>imdbRating: </span> ${imdbRating}</p>
+                <P><span>Genre: </span> ${Genre}</p>
+                <P><span>Language: </span> ${Language}</p>
+                <P> <span> Plot: </span> ${Plot}</p>
+                </div>
                 <img class="poster" src="${Poster}" alt="${Title} Poster">
-                    <h2 class="title">${Title} (${Year})</h2>
-                    <button class="watchBtn">Watch Now</button>
+                `;
+
+                mMovieCards.innerHTML = `
+                <div class="m-movieDetails">
+                <h2 class="title">${Title} (${Year})</h2>
+                <img class="poster" src="${Poster}" alt="${Title} Poster">
+                <P><span>Actors: </span> ${Actors}</p>
+                <P><span>Director: </span> ${Director}</p>
+                <P><span>Writer: </span> ${Writer}</p>
+                <P><span>Released: </span> ${Released}</p>
+                <P><span>imdbRating: </span> ${imdbRating}</p>
+                <P><span>Genre: </span> ${Genre}</p>
+                <P><span>Language: </span> ${Language}</p>
+                <P> <span> Plot: </span> ${Plot}</p>
+                </div>
                 `;
                 console.log(data)
                 console.log("Yes")
             } else {
-                movieDetails.innerHTML = '<p>Movie not found</p>';
+                movieDetails.innerHTML = '<p class="not-found">Movie not found</p>';
             }
         })
         .catch(error => {
